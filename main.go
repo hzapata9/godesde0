8c2a8058f9283a65906a7606ce43cae062ac6d2c
Hector Zapata <hezapata@falabella.cl>
2024-01-02 19:24:04 -0300

    Segundo commit

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/hzapata9/godesde0/variables"
 )
 
 func main() {
 	variables.MuestraEntyeros()
+
+	variables.RestoVariables()
+
+	estado, texto := variables.ConviertoaTexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
